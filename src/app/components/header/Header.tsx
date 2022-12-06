3ffad7ccebcd27c796c4d07da3314d8af49a7dc2
skif-cezar <skif.cezar@gmail.com>
2022-12-07 00:11:08 +0300

    fix: fix active link

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import clsx from "clsx";
 import styles from "src/app/components/header/Header.module.scss";
 import {Menu} from "src/app/components/menu/Menu";
+import {animateScroll as scroll} from "react-scroll";
 
 /**
  * Header component
@@ -12,10 +13,24 @@ export const Header: React.FC = () => {
   const HEADER_LOGO_STYLES = clsx(styles.header__logo);
   const HEADER_PHONE_STYLES = clsx(styles.header__phone);
 
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>): void => {
+    if (event.key === "Enter") {
+      scroll.scrollToTop();
+    }
+  };
+
   return (
     <header className={HEADER_STYLES}>
       <div className={HEADER_NAVIGATION_STYLES}>
-        <div className={HEADER_LOGO_STYLES}>
+        <div
+          className={HEADER_LOGO_STYLES}
+          onClick={() => {
+            return scroll.scrollToTop();
+          }}
+          tabIndex={0}
+          onKeyDown={handleKeyDown}
+          role="button"
+        >
           LOGO
         </div>
         <Menu />
