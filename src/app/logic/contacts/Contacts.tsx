d673e6ae31be6aa539fba84d5a501f37647a6c22
skif-cezar <skif.cezar@gmail.com>
2022-12-25 01:29:38 +0300

    feat: add form component

--- src/app/logic/contacts/Contacts.tsx
+++ src/app/logic/contacts/Contacts.tsx
@@ -1,14 +1,7 @@
 import React from "react";
-import {Message, useForm} from "react-hook-form";
 import clsx from "clsx";
 import styles from "src/app/logic/contacts/Contacts.module.scss";
-import {Button} from "src/app/components/button/Button";
-
-export type FieldsForm = {
-  name?: string;
-  message?: Message;
-  phone?: number;
-};
+import {Form} from "src/app/components/form/Form";
 
 /**
  * Contacts section
@@ -18,20 +11,7 @@ export const Contacts: React.FC = () => {
   const CONTENT_STYLES = clsx(styles.contacts__content);
   const CONTAINER_STYLES = clsx(styles.contacts__container);
   const CONTENT_TITLE_STYLES = clsx(styles.contacts__title);
-  const FORM_STYLES = clsx(styles.form);
-  const FORM_ERRORS_STYLES = clsx(styles.form__errors);
   const CONTACTS_ADRESS_STYLES = clsx(styles.contacts__adress);
-
-  // eslint-disable-next-line @typescript-eslint/typedef
-  const {register, handleSubmit, watch, reset, formState: {errors}} = useForm<FieldsForm>({mode: "onBlur"});
-  const onSubmit = (data: any): void => {
-    // eslint-disable-next-line no-console
-    console.log(data);
-    reset();
-  };
-
-  // eslint-disable-next-line no-console
-  console.log(watch("name"));
 
   return (
     <section className={CONTACTS_STYLES} id="contacts">
@@ -42,33 +22,7 @@ export const Contacts: React.FC = () => {
             Оставьте свои контакты. Менеджер перезвонит в&nbsp;удобное вам время и ответит на все
             вопросы.
           </p>
-          <form className={FORM_STYLES} onSubmit={handleSubmit(onSubmit)}>
-            <input
-              {...register("name", {
-                minLength: {
-                  value: 2,
-                  message: "Минимум 2 символа",
-                },
-                required: "Поле обязательно к заполнению",
-              })}
-              type="text"
-              placeholder="Ваше имя"
-            />
-            {errors["name"] && <span className={FORM_ERRORS_STYLES}>{errors["name"].message}</span>}
-            <input
-              {...register("phone", {
-                pattern: {
-                  value: /^\+?([0-9]{3})\)?[-. ]?([0-9]{2})[-. ]?([0-9]{3})[-. ]?([0-9]{2})[-. ]?([0-9]{2})$/,
-                  message: "Номер должен быть формата: \"+375-XX-XXX-XX-XX\"",
-                },
-                required: "Поле обязательно к заполнению",
-              })}
-              type="tel"
-              placeholder="Введите телефон"
-            />
-            {errors["phone"] && <span className={FORM_ERRORS_STYLES}>{errors["phone"].message}</span>}
-            <Button text="Оставить заявку" />
-          </form>
+          <Form />
         </article>
         <div className={CONTACTS_ADRESS_STYLES}>
           <iframe
